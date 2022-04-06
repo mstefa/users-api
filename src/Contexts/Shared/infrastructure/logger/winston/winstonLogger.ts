602c4bc607a9aa72181f6e9dc99d68e5204d50be
mstefanutti <matias.stefanutti@settlenetwork.com>
2022-04-06 14:06:59 -0300

    feat: add json format to logs

--- src/Contexts/Shared/infrastructure/logger/winston/winstonLogger.ts
+++ src/Contexts/Shared/infrastructure/logger/winston/winstonLogger.ts
@@ -2,29 +2,25 @@ import winston, { createLogger, format, transports} from 'winston';
 import  'winston-daily-rotate-file';
 import moocConfig from '../../../../Auth/Shared/infrastructure/config';
 
-const myFormat = format.printf(({ level, message, labels, timestamps }) =>
-    `${timestamps} [${labels}] ${level}: ${message}`);
+// const myFormat = format.printf(({ level, message, timestamps }) =>
+//     `${timestamps} ${level}: ${message}`);
 
 export class Log {
   private static WinstonLogger = createLogger({
     format: format.combine(
-      format.label({ label: moocConfig.get('env') }), //{ label: 'right meow!' }
+      format.label({ label: moocConfig.get('env') }),
       format.timestamp(),
-      myFormat
+      format.json()
     ),
     transports: [
-      new transports.Console({
-        format: format.combine(
-          format.timestamp(),
-          format.colorize(),
-        )
-      }),
+      new transports.Console(),
       new winston.transports.DailyRotateFile({
         filename: './logs/application-%DATE%.log',
         datePattern: 'YYYY-MM-DD-HH',
         zippedArchive: true,
         maxSize: '20m',
-        maxFiles: '14d'
+        maxFiles: '14d',
+
       })
     ]
   });
